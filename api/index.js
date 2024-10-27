28c9a1a55d3a488c7095a768d8ee02e439fd2766
phoenix-flyhigh <j.akshaya07@gmail.com>
2024-10-27 21:09:20 +0530

    Add preview player with spotify track previews

--- api/index.js
+++ api/index.js
@@ -1,52 +1,108 @@
-const express = require("express")
-const path = require("path")
-const fs = require("fs")
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const axios = require("axios");
+require("dotenv").config();
 
-const PORT = 8080
+const client_id = process.env.SPOTIFY_CLIENT_ID;
+const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+const TOTAL_TRACKS = 3;
+const token_url = "https://accounts.spotify.com/api/token";
+const track_url =
+  "https://api.spotify.com/v1/playlists/3cEYpjA9oz9GiPac4AsH4n/tracks";
 
-const app = express()
-const staticPath = path.join(__dirname, "../public")
-
-app.use(express.static(staticPath))
+const app = express();
+const staticPath = path.join(__dirname, "../public");
+app.use(express.static(staticPath));
 
 const getJsonDataFromFile = (filePath, res) => {
-    fs.readFile(filePath, "utf-8", (err, data) => {
-        if (err){
-            return res.status(500).json({error: "Failed to read data from file"})
-        }
-        try{
-            const jsonData = JSON.parse(data)
-            res.json(jsonData)
-        }
-        catch(e){
-            res.status(500).json({error: "Failed to parse json data"})
-        }
-    })
-}
+  fs.readFile(filePath, "utf-8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Failed to read data from file" });
+    }
+    try {
+      const jsonData = JSON.parse(data);
+      res.json(jsonData);
+    } catch (e) {
+      res.status(500).json({ error: "Failed to parse json data" });
+    }
+  });
+};
 
 app.get("/artists", (_, res) => {
-    const filePath = path.join(__dirname, "data/artists.json")
+    const filePath = path.join(__dirname, "data/artists.json");
+  
+    getJsonDataFromFile(filePath, res);
+  });
+  
+  app.get("/albums", (_, res) => {
+    const filePath = path.join(__dirname, "data/albums.json");
+  
+    getJsonDataFromFile(filePath, res);
+  });
+  
+  app.get("/charts", (_, res) => {
+    const filePath = path.join(__dirname, "data/charts.json");
+  
+    getJsonDataFromFile(filePath, res);
+  });
 
-    getJsonDataFromFile(filePath, res)
-})
 
-app.get("/albums", (_, res) => {
-    const filePath = path.join(__dirname, "data/albums.json")
+let accessToken = null
+let tokenExpiration = null
 
-    getJsonDataFromFile(filePath, res)
-})
+const fetchAccessToken = async () => {
+  try {
+    const response = await axios.post(token_url, null, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      params: {
+        grant_type: "client_credentials",
+        client_id: `${client_id}`,
+        client_secret: `${client_secret}`,
+      },
+    });
+    accessToken = response.data.access_token
+    tokenExpiration = Date.now() + response.data.expires_in * 1000; // Set expiration time
+  } catch (error) {
+    console.error("Error fetching access token:", error);
+    throw new Error("Could not fetch access token");
+  }
+};
 
-app.get("/charts", (_, res) => {
-    const filePath = path.join(__dirname, "data/charts.json")
+app.get("/tracks", async (_, res) => {
+  if (!accessToken || Date.now() >= tokenExpiration) {
+    try {
+      await fetchAccessToken(); 
+    } catch (err) {
+      return res.status(500).json({ error: "Failed to fetch access token" });
+    }
+  }
+  try {
+    const response = await axios.get(track_url, {
+      headers: {
+        Authorization: "Bearer " + accessToken,
+      },
+      params: {
+        limit: TOTAL_TRACKS,
+        fields:'items(track(id,name,preview_url,album(images),artists(name)))'
+      },
+    });
 
-    getJsonDataFromFile(filePath, res)
-})
+    const data = response.data;
+    return res.json(data);
+  } catch (err) {
+    console.error("failed to get tracks");
+    return res.status(500).json({ error: "Failed to fetch tracks" });
+  }
+});
 
 if (require.main === module) {
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}`);
-    });
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
 }
 
 module.exports = app;
--- public/index.js
+++ public/index.js
@@ -1,54 +1,83 @@
-console.log("logged");
-let artistData = []
-let albumData = []
-let chartsData = []
-let initialNumberOfImagesToFit = 0;
+(() => {
+  let artistData = [];
+  let albumData = [];
+  let chartsData = [];
+  let initialNumberOfImagesToFit = 0;
+  let currentTrackToPlay = new Audio();
+  let TOTAL_TRACKS = 3;
+  const container = document.getElementById("main-content");
+  let trackdata = [];
+  let currentTrackId = 0;
 
-const container = document.getElementById("main-content")
+  const getNumberOfImagesToFitContainer = () => {
+    const containerWidth = container.offsetWidth;
+    const imageWidth = 240;
+    const margin = 20;
 
-const getNumberOfImagesToFitContainer = () => {
-    const containerWidth = container.offsetWidth
-    const imageWidth = 240 
-    const margin = 20
-
-    const numberOfImages = Math.floor(containerWidth / (imageWidth + margin))
+    const numberOfImages = Math.floor(containerWidth / (imageWidth + margin));
     return numberOfImages;
-}
+  };
 
-fetch("/artists").then((res) => res.json()) .then((data) => {
-    console.log("artist data", data)
-    artistData = data
+  fetch("/artists")
+    .then((res) => res.json())
+    .then((data) => {
+      artistData = data;
 
-    if(!initialNumberOfImagesToFit){
-        initialNumberOfImagesToFit = getNumberOfImagesToFitContainer()
-    }
-    showArtists(artistData.slice(0, Math.min(initialNumberOfImagesToFit, artistData.length)))
-})
+      if (!initialNumberOfImagesToFit) {
+        initialNumberOfImagesToFit = getNumberOfImagesToFitContainer();
+      }
+      showArtists(
+        artistData.slice(
+          0,
+          Math.min(initialNumberOfImagesToFit, artistData.length)
+        )
+      );
+    });
 
-fetch("/albums").then((res) => res.json()) .then((data) => {
-    console.log("album data", data)
-    albumData = data
-    if(!initialNumberOfImagesToFit){
-        initialNumberOfImagesToFit = getNumberOfImagesToFitContainer()
-    }
-    showAlbums(albumData.slice(0, Math.min(initialNumberOfImagesToFit, albumData.length)))
-})
+  fetch("/albums")
+    .then((res) => res.json())
+    .then((data) => {
+      albumData = data;
+      if (!initialNumberOfImagesToFit) {
+        initialNumberOfImagesToFit = getNumberOfImagesToFitContainer();
+      }
+      showAlbums(
+        albumData.slice(
+          0,
+          Math.min(initialNumberOfImagesToFit, albumData.length)
+        )
+      );
+    });
 
-fetch("/charts").then((res) => res.json()) .then((data) => {
-    console.log("charts data", data)
-    chartsData = data
-    if(!initialNumberOfImagesToFit){
-        initialNumberOfImagesToFit = getNumberOfImagesToFitContainer()
-    }
-    showCharts(chartsData.slice(0, Math.min(initialNumberOfImagesToFit, chartsData.length)))
-})
+  fetch("/charts")
+    .then((res) => res.json())
+    .then((data) => {
+      chartsData = data;
+      if (!initialNumberOfImagesToFit) {
+        initialNumberOfImagesToFit = getNumberOfImagesToFitContainer();
+      }
+      showCharts(
+        chartsData.slice(
+          0,
+          Math.min(initialNumberOfImagesToFit, chartsData.length)
+        )
+      );
+    });
 
-const showArtists = (artistData) => {
-    const artistSection = document.getElementById("popular-artists")
-    artistData.forEach(artist => {
-        const element = document.createElement("div")
-        element.classList.add("flex", "flex-col", "gap-2", "card", "p-4", "rounded-md", "relative")
-        element.innerHTML = `
+  const showArtists = (artistData) => {
+    const artistSection = document.getElementById("popular-artists");
+    artistData.forEach((artist) => {
+      const element = document.createElement("div");
+      element.classList.add(
+        "flex",
+        "flex-col",
+        "gap-2",
+        "card",
+        "p-4",
+        "rounded-md",
+        "relative"
+      );
+      element.innerHTML = `
             <img
                 src=${artist.url}
                 class="w-24 h-24 min-w-16 min-h-16 rounded-full"
@@ -60,17 +89,25 @@ const showArtists = (artistData) => {
                 <img src="assets/play.svg" class="" alt="browse" />
             </button>
         `;
-        
-        artistSection.appendChild(element)
-    })
-}
 
-const showAlbums = (albumData) => {
-    const albumSection = document.getElementById("popular-albums")
-    albumData.forEach(album => {
-        const element = document.createElement("div")
-        element.classList.add("flex", "flex-col", "gap-2", "card", "p-4", "rounded-sm", "relative")
-        element.innerHTML = `
+      artistSection.appendChild(element);
+    });
+  };
+
+  const showAlbums = (albumData) => {
+    const albumSection = document.getElementById("popular-albums");
+    albumData.forEach((album) => {
+      const element = document.createElement("div");
+      element.classList.add(
+        "flex",
+        "flex-col",
+        "gap-2",
+        "card",
+        "p-4",
+        "rounded-sm",
+        "relative"
+      );
+      element.innerHTML = `
             <img
                 src=${album.url}
                 class="w-24 h-24 min-w-16 min-h-16"
@@ -82,17 +119,27 @@ const showAlbums = (albumData) => {
                 <img src="assets/play.svg" class="" alt="browse" />
             </button>
         `;
-        
-        albumSection.appendChild(element)
-    })
-}
 
-const showCharts = (chartsData) => {
-    const chartsSection = document.getElementById("featured-charts")
-    chartsData.forEach(chart => {
-        const element = document.createElement("div")
-        element.classList.add("flex", "flex-col", "gap-2", "card", "p-4", "rounded-sm", "relative", "justify-center",  "items-center")
-        element.innerHTML = `
+      albumSection.appendChild(element);
+    });
+  };
+
+  const showCharts = (chartsData) => {
+    const chartsSection = document.getElementById("featured-charts");
+    chartsData.forEach((chart) => {
+      const element = document.createElement("div");
+      element.classList.add(
+        "flex",
+        "flex-col",
+        "gap-2",
+        "card",
+        "p-4",
+        "rounded-sm",
+        "relative",
+        "justify-center",
+        "items-center"
+      );
+      element.innerHTML = `
             <img
                 src=${chart.url}
                 class="w-24 h-24 min-w-16 min-h-16"
@@ -103,16 +150,289 @@ const showCharts = (chartsData) => {
                 <img src="assets/play.svg" class="" alt="browse" />
             </button>
         `;
-        
-        chartsSection.appendChild(element)
-    })
-}
 
-const adjustVisibleImages = () => {
-    const numberOfImages =getNumberOfImagesToFitContainer()
-    showArtists(artistData.slice(0, Math.min(numberOfImages, artistData.length)))
-    showAlbums(albumData.slice(0, Math.min(numberOfImages, albumData.length)))
-    showCharts(chartsData.slice(0, Math.min(numberOfImages, chartsData.length)))
-}
+      chartsSection.appendChild(element);
+    });
+  };
 
-window.addEventListener("resize", adjustVisibleImages)
+  const adjustVisibleImages = () => {
+    const numberOfImages = getNumberOfImagesToFitContainer();
+    showArtists(
+      artistData.slice(0, Math.min(numberOfImages, artistData.length))
+    );
+    showAlbums(albumData.slice(0, Math.min(numberOfImages, albumData.length)));
+    showCharts(
+      chartsData.slice(0, Math.min(numberOfImages, chartsData.length))
+    );
+  };
+
+  window.addEventListener("resize", adjustVisibleImages);
+
+  const footer = document.querySelector(".footer");
+  const player = document.querySelector(".player");
+  const loader = document.querySelector(".loader");
+  const previewBtn = document.getElementById("preview");
+
+  const showMusicPlayer = async () => {
+    footer.style.display = "none";
+    loader.style.display = "flex";
+
+    await fetch("/tracks")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        loader.style.display = "none";
+        player.style.display = "flex";
+        trackdata = data.items;
+        showTrackDetails();
+      })
+      .catch(() => {
+        console.error("failed to get tracks");
+      });
+  };
+
+  previewBtn?.addEventListener("click", showMusicPlayer);
+
+  const playBtn = document.getElementById("play-btn");
+  const pauseBtn = document.getElementById("pause-btn");
+
+  playBtn?.addEventListener("click", () => {
+    if (currentTrackToPlay.src) {
+      playBtn.classList.add("hidden");
+      pauseBtn.classList.remove("hidden");
+      pauseBtn.classList.add("flex");
+      currentTrackToPlay.play();
+    }
+  });
+
+  pauseBtn?.addEventListener("click", () => {
+    if (currentTrackToPlay.src) {
+      playBtn.classList.remove("hidden");
+      playBtn.classList.add("flex");
+      pauseBtn.classList.add("hidden");
+      currentTrackToPlay.pause();
+    }
+  });
+
+  const showTrackDetails = () => {
+    const currentTrack = trackdata[currentTrackId].track;
+    const name = document.getElementById("track-name");
+    const artist = document.getElementById("track-artist");
+    const image = document.getElementById("track-image");
+
+    image.src = currentTrack.album.images[2].url;
+    name.innerHTML = currentTrack.name;
+    artist.innerHTML = currentTrack.artists[0].name;
+    currentTrackToPlay.src = currentTrack.preview_url;
+  };
+
+  const getProgress = (e, rect) => {
+    const offset = e.clientX - rect.left;
+    const percentage = Math.min(Math.max(offset / rect.width, 0), 1);
+    return percentage;
+  };
+
+  const getSecondsFromTime = (time) => {
+    const timeParts = time.split(":");
+    const seconds = Number(timeParts[0]) * 60 + Number(timeParts[1]);
+    return seconds;
+  };
+
+  const convertSecondsToTime = (totalSeconds) => {
+    const roundedTimeInSeconds = Math.round(totalSeconds);
+    const minutes = Math.floor(roundedTimeInSeconds / 60);
+    const seconds = roundedTimeInSeconds % 60;
+    let formattedTime = ``;
+    minutes < 10
+      ? (formattedTime += `0${minutes}`)
+      : (formattedTime += `${minutes}`);
+    formattedTime += `:`;
+    seconds < 10
+      ? (formattedTime += `0${seconds}`)
+      : (formattedTime += `${seconds}`);
+    return formattedTime;
+  };
+
+  const trackBar = document.getElementById("track-bar");
+  const trackProgress = document.getElementById("track-progress");
+  const trackDragger = document.getElementById("track-notch");
+  const trackElapsedTime = document.getElementById("elapsed-time");
+  const trackTotalTime = document.getElementById("total-time");
+  let isDraggingTrack = false;
+
+  const updateTrackProgress = (e) => {
+    const rect = trackBar.getBoundingClientRect();
+    const percentage = getProgress(e, rect);
+    const newWidth = percentage * rect.width;
+
+    trackProgress.style.width = `${newWidth}px`;
+    trackDragger.style.left = `${newWidth}px`;
+
+    const totalTime = trackTotalTime.innerHTML;
+    const totalDurationInSeconds = getSecondsFromTime(totalTime);
+    const elapsedTimeInSeconds = percentage * totalDurationInSeconds;
+    const elapsedTime = convertSecondsToTime(elapsedTimeInSeconds);
+
+    trackElapsedTime.innerText = elapsedTime;
+    currentTrackToPlay.currentTime = percentage * currentTrackToPlay.duration;
+  };
+
+  trackBar.addEventListener("click", updateTrackProgress);
+
+  trackBar.addEventListener("mousedown", (e) => {
+    isDraggingTrack = true;
+    trackBar.classList.add("dragging-track");
+    updateTrackProgress(e);
+  });
+
+  trackBar.addEventListener("mouseup", () => {
+    isDraggingTrack = false;
+    trackBar.classList.remove("dragging-track");
+  });
+
+  document.addEventListener("mousemove", (e) => {
+    if (isDraggingTrack) {
+      updateTrackProgress(e);
+    }
+  });
+
+  document.addEventListener("mouseup", () => {
+    if (isDraggingTrack) {
+      isDraggingTrack = false;
+      trackBar.classList.remove("dragging-track");
+    }
+  });
+
+  currentTrackToPlay.addEventListener("timeupdate", () => {
+    const percentage =
+      currentTrackToPlay.currentTime / currentTrackToPlay.duration;
+    const rect = trackBar.getBoundingClientRect();
+
+    const newWidth = percentage * rect.width;
+
+    trackProgress.style.width = `${newWidth}px`;
+    trackDragger.style.left = `${newWidth}px`;
+    trackElapsedTime.innerText = `${convertSecondsToTime(
+      currentTrackToPlay.currentTime
+    )}`;
+  });
+
+  currentTrackToPlay.addEventListener("ended", () => {
+    if (currentTrackId < TOTAL_TRACKS - 1) {
+      currentTrackId += 1;
+    } else {
+      currentTrackId = 0;
+    }
+    showTrackDetails();
+    trackProgress.style.width = `0px`;
+    trackDragger.style.left = `0px`;
+    trackElapsedTime.innerText = `00:00`;
+    if (currentTrackId != 0) {
+      currentTrackToPlay.play();
+    } else {
+      pauseBtn.classList.add("hidden");
+      playBtn.classList.remove("hidden");
+      playBtn.classList.add("flex");
+    }
+  });
+
+  const volumeBtn = document.querySelector("#volume-btn");
+  const highVolumeImg = document.querySelector(".high-vol");
+  const lowVolumeImg = document.querySelector(".low-vol");
+  const zeroVolumeImg = document.querySelector(".zero-vol");
+
+  const volumeBar = document.getElementById("volume-bar");
+  const volumeProgress = document.getElementById("volume-progress");
+  const volumeNotch = document.getElementById("volume-notch");
+
+  let currentVolumePercentage = 1;
+  let previousVolumePercentage = 1;
+  const VOLUME_THRESHOLD = 0.7;
+
+  const showVolumeBtn = (element) => {
+    highVolumeImg.style.opacity = 0;
+    lowVolumeImg.style.opacity = 0;
+    zeroVolumeImg.style.opacity = 0;
+
+    element.style.opacity = 1;
+  };
+
+  const setVolumeProgress = (percentage) => {
+    const volumeBarRect = volumeBar.getBoundingClientRect();
+
+    volumeProgress.classList.remove("w-full");
+    volumeProgress.style.width = `${Math.round(percentage * volumeBarRect.width)}px`;
+    volumeNotch.style.left = `${percentage * 100}%`;
+  };
+
+  volumeBtn.addEventListener("click", () => {
+    if (currentVolumePercentage > 0) {
+      // mute the song
+      showVolumeBtn(zeroVolumeImg);
+      setVolumeProgress(0);
+      currentTrackToPlay.volume = 0;
+      previousVolumePercentage = currentVolumePercentage;
+      currentVolumePercentage = 0;
+    } else { // set volume to previous volume before muting
+      currentVolumePercentage = previousVolumePercentage;
+      previousVolumePercentage = 0;
+      
+      setVolumeProgress(currentVolumePercentage);
+      currentTrackToPlay.volume = currentVolumePercentage;
+
+      if (currentVolumePercentage > VOLUME_THRESHOLD) {
+        showVolumeBtn(highVolumeImg);
+      } else {
+        showVolumeBtn(lowVolumeImg);
+      }
+    }
+  });
+
+  let isDraggingVolume = false;
+
+  const updateVolumeProgress = (e) => {
+    const volumeBarRect = volumeBar.getBoundingClientRect();    
+    const percentage = getProgress(e, volumeBarRect);
+    
+    if(percentage == 0){
+      showVolumeBtn(zeroVolumeImg);
+    }
+    else if (percentage > VOLUME_THRESHOLD){
+      showVolumeBtn(highVolumeImg);
+    }
+    else {
+      showVolumeBtn(lowVolumeImg);
+    }
+    setVolumeProgress(percentage);
+    currentTrackToPlay.volume = percentage;
+    currentVolumePercentage = percentage;
+  };
+
+  volumeBar.addEventListener("click", updateVolumeProgress);
+
+  volumeBar.addEventListener("mousedown", (e) => {
+    isDraggingVolume = true;
+    volumeBar.classList.add("dragging-volume");
+    updateVolumeProgress(e);
+  });
+
+  volumeBar.addEventListener("mouseup", () => {
+    isDraggingVolume = false;
+    volumeBar.classList.remove("dragging-volume");
+  });
+
+  document.addEventListener("mousemove", (e) => {
+    if (isDraggingVolume) {
+      updateVolumeProgress(e);
+    }
+  });
+
+  document.addEventListener("mouseup", () => {
+    if (isDraggingVolume) {
+      isDraggingVolume = false;
+      volumeBar.classList.remove("dragging-volume");
+    }
+  });
+
+})();
